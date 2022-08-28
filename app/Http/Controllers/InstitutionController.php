eec309d2a8a3d8eb1200a51edb0de50eaafeb045
ClydeLiu1 <clydeliu1@gmail.com>
2022-08-29 09:53:55 +1200

    fristproject

---
+++ app/Http/Controllers/InstitutionController.php
@@ -0,0 +1,77 @@
+<?php
+//use App\Http\Controllers\InstitutionController; // If you do not add this statement, you will 
+// not have access to the action methods in this controller
+namespace App\Http\Controllers;
+use App\Models\Institution;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+class InstitutionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Institution::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {  
+        
+        $request->validate([
+        'name' => 'required',
+        'region' => 'required',
+        'country' => 'required'
+    ]);
+
+        return Institution::create($request->all());
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Institution::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $institution = Institution::find($id);
+        $institution->update($request->all());
+        return $institution;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Institution::destroy($id);
+
+    }
+}
